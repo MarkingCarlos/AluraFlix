b0f1e361b3edfb8045103fdf3d78c2d881aa51c9
Carlos Alberto <carlos.a.souza@unesp.br>
2023-10-31 22:33:27 -0300

    first

---
+++ src/components/Menu/Menu.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Logo from '../../assets/img/carflix.png'
+import './menu.css'
+import ButtonLink from "../Button/ButtonLink";
+function Menu(){
+    
+    return (
+        <nav className="Menu">
+            <a href="/">
+            <img className="Logo" src={Logo} alt="CarFlix Logo"/>
+            </a>
+
+            <ButtonLink className="ButtonLink" href="/">
+                Novo video
+            </ButtonLink>
+        </nav>
+    );
+}
+
+export default Menu;
